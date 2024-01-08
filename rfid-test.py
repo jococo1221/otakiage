ed87180f960296ceaa863f0ce22520832d63df29
jococo1221 <jococo1221@gmail.com>
2024-01-08 14:42:52 +0000

    Added dictionary of tags

--- rfid-test.py
+++ rfid-test.py
@@ -4,7 +4,6 @@
 import signal
 import time
 import sys
-#sys.path.insert(0, '/usr/lib/python3/dist-packages/pirc522')
 
 from pirc522 import RFID
 
@@ -15,8 +14,25 @@ rdr = RFID()
 util = rdr.util()
 util.debug = True
 
-tag1 = [0x09, 0x37, 0x1A, 0xE5, 0xC1]  # Replace with the UID of tag1 09,37,1a,e5,c1
-tag2 = [0xC9, 0x00, 0xFA, 0xB9, 0x8A]  # Replace with the UID of tag2 c9,00,fa,b9,8a
+class RFIDTag:
+    def __init__(self, uid, message):
+        self.uid = uid
+        self.message = message
+
+def detect_tag(uid):
+    for key, rfid_tag in rfid_tags.items():
+        if uid == rfid_tag.uid:
+            return key, rfid_tag.message
+    return None, f"Unknown tag detected with UID: [{', '.join([f'0x{hex(x)[2:].zfill(2)}' for x in uid])}]"
+
+
+rfid_tags = {
+    1: RFIDTag(uid=[0x09, 0x37, 0x1A, 0xE5, 0xC1], message="Blue tag 1"),
+    2: RFIDTag(uid=[0xC9, 0x00, 0xFA, 0xB9, 0x8A], message="Blue tag 2"),
+    3: RFIDTag(uid=[0x88, 0x04, 0xC3, 0x8A, 0xC5], message="Sticker 1"),
+    4: RFIDTag(uid=[0x88, 0x04, 0xbb, 0x8a, 0xbd], message="Sticker 2"),
+    5: RFIDTag(uid=[0x62, 0xbb, 0x09, 0x1a, 0xca], message="White card"),
+}
 
 def end_read(signal, frame):
     global run
@@ -36,25 +52,22 @@ while run:
 
     (error, uid) = rdr.anticoll()
     if not error:
-        #print("Card read UID: " + str(uid[0]) + "," + str(uid[1]) + "," +
-        #		str(uid[2]) + "," + str(uid[3]))
 
-        uid_hex = [hex(x)[2:].zfill(2) for x in uid]
-        print("Card read UID: " + ",".join(uid_hex))
+        tag_found = False
 
-        if uid == tag1:
-            print("Tag 1 detected. Outputting Message1")
-            # Add the actions for tag1 detection here
-        elif uid == tag2:
-            print("Tag 2 detected. Outputting Message2")
-            # Add the actions for tag2 detection here
+        detected_uid=uid
+
+        tag_key, tag_message = detect_tag(detected_uid)
+
+        if tag_key is not None:
+            print(f"Tag {tag_key} detected. {tag_message}")
+            # Add the actions for tag detection here
         else:
-            print("Unknown tag detected")
+            print(tag_message)       
 
 
 print("Setting tag")
 util.set_tag(uid)
-
 
 
 print("\nAuthorizing")
